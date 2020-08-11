8cc0471aac63f9217077aa22aa76b301d260fed9
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-11 14:47:57 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(this_persons_name)
+    @name = this_persons_name
+  end 
+  
+  def name 
+    @name
+  end 
+
+  def job=(job_type)
+    @this_persons_job = job_type
+  end 
+  
+  def job 
+    @job = job_type
+  end 
+end 
