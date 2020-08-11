1ae60408809cda997e3dc2b7e8d3071029b05d25
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-11 14:45:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,24 +4,15 @@ class Dog
   end
   
   def name
-    @name = dog_name
+    @name 
   end 
 
   def breed=(breed_type)
-    @this_dogs_breed = breed_type
+    @breed = breed_type
   end
   
   def breed 
-    @breed = breed_type
+    @breed 
   end 
 end 
 
-class Person
-  def job=(job_type)
-    @this_persons_job = job_type
-  end 
-  
-  def job 
-    @job = job_type
-  end 
-end 
